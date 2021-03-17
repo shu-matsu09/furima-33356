4f9aade7291df787329c69a14409467633e1eb8d
shu-matsu09 <shusukem1988@gmail.com>
2021-03-17 17:31:38 +0900

    モデルテストに必要なGemの導入

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    gimei = Gimei.name
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name             {gimei.last.kanji}
+    first_name            {gimei.first.kanji}
+    last_name_kana        {gimei.last.katakana}
+    first_name_kana       {gimei.first.katakana}
+    birth_date            {Faker::Date.between(from: '1930-01-01', to: 5.years.ago)}
+  end
+end
