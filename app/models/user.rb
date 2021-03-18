08841a6d35148c69799c2bd1394f4a0237d1229e
shu-matsu09 <shusukem1988@gmail.com>
2021-03-18 15:03:00 +0900

    Userモデルテスト3回目

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,19 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方(半角)を含めて設定してください'
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'には全角文字(漢字・ひらがな・カタカナ)を使用してください' } do
-    validates :last_name
-    validates :first_name
+  with_options presence: true do
+    validates :nickname
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'には全角文字(漢字・ひらがな・カタカナ)を使用してください' } do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶー]+\z/, message: 'には全角(カタカナ)を使用してください' } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+    validates :birth_date
   end
-  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'には全角(カタカナ)を使用してください' } do
-    validates :last_name_kana
-    validates :first_name_kana
-  end
-  validates :birth_date, presence: true
 end
