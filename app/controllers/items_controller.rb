0251b4556f1f3afc1e002edb03d29bb6834163e8
shu-matsu09 <shusukem1988@gmail.com>
2021-03-29 11:49:57 +0900

    コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render new_item_path
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :delivery_fee_id, :predectures_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,12 +10,12 @@ class Item < ApplicationRecord
       validates :days_to_ship_id
     end
     validates :price, numericality: {
-       only_integer: true, :greater_than_or_equal_to: 300, :less_than_or_equal_to: 9999999 
+       only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 
       }
   end
 
   belongs_to :user
-  extend ActiveHash::Associations::ApplicationRecordExtensions
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category_id
   belongs_to :condition_id
   belongs_to :delivery_fee_id
