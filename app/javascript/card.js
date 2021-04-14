8a43ac95b688ef1671c0b999a4eedf33e771a0a5
shu-matsu09 <shusukem1988@gmail.com>
2021-04-15 08:56:06 +0900

    環境変数の設定

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_f4857756c77540fbb921deca")
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
