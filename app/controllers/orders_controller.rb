3bfe15b65d2d668c57435b71338656fbf9082793
shu-matsu09 <shusukem1988@gmail.com>
2021-04-14 19:47:53 +0900

    ordersコントローラーの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,24 @@
 class OrdersController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
+    @item_order = ItemOrder.new
   end
+
+  def create
+    @item_order = ItemOrder.new(order_params)
+      if @item_order.valid?
+        @item_order.save
+        redirect_to root_path
+      else
+        render action: :index
+      end
+  end
+
+  private
+
+  def order_params
+    params.require(:item_order).permit(:postal_code, :prefectures, :municipality, :house_number, :building_name, :phone_number)
+  end
+
+  
 end
