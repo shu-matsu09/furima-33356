6164da06660214ea38b80520ba9346746e698768
shu-matsu09 <shusukem1988@gmail.com>
2021-07-03 10:50:57 +0900

    item_orderモデルの単体テスト修正

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -7,11 +7,11 @@ class ItemOrder
     validates :user_id
     validates :item_id
     validates :token
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :prefectures_id, numericality: { other_than: 1, message: 'Select' }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'は半角数字で、「三桁の数字-四桁の数字」の形で入力してください' }
+    validates :prefectures_id, numericality: { other_than: 1, message: 'を選択してください' }
     validates :municipality
     validates :house_number
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is out of setting range' }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'は半角数字10~11桁で入力してください' }
   end
 
   def save
